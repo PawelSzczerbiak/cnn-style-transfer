4fd042cb12217af870c240895f2093ab56fd3584
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2019-05-16 08:23:45 +0200

    Gram matrix, parameters initialization

--- helper_functions.py
+++ helper_functions.py
@@ -11,65 +11,65 @@ from io import BytesIO
 
 
 def load_vgg19(pretrained=True, requires_grad=False, only_features=True):
-	'''Returns vgg19 model with some options to be customized'''
+    '''Returns vgg19 model with some options to be customized'''
 
-	model = models.vgg19(pretrained=pretrained)
+    model = models.vgg19(pretrained=pretrained)
 
-	if not requires_grad:
-		for param in model.parameters():
-			param.requires_grad_(False)
+    if not requires_grad:
+        for param in model.parameters():
+            param.requires_grad_(False)
 
-	if only_features:
-		return model.features
+    if only_features:
+        return model.features
 
-	return model
+    return model
 
 
 def load_image(path, max_size=256, shape=None):
-	'''Load in and transform an image 
-	   with max_size not larger than specified
-	   or a given shape (if specified)
+    '''Load in and transform an image 
+       with max_size not larger than specified
+       or a given shape (if specified)
     '''
 
-	if 'http' in path:
-		respone = requests.get(path)
-		image = Image.open(BytesIO(response.content)).convert('RGB')
-	else:
-		image = Image.open(path).convert('RGB')
+    if 'http' in path:
+        respone = requests.get(path)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(path).convert('RGB')
 
-	if shape is not None:
-		size = shape
-	else:
-		# resize height and width to be smaller than max_size
-		w, h = image.size
-		if h >= max(w, max_size):
-			h, w = (max_size, int(w * max_size / h))
-		elif w >  max(h, max_size):
-			h, w = (int(h * max_size / w), max_size)
-		size = (h, w)
+    if shape is not None:
+        size = shape
+    else:
+        # resize height and width to be smaller than max_size
+        w, h = image.size
+        if h >= max(w, max_size):
+            h, w = (max_size, int(w * max_size / h))
+        elif w >  max(h, max_size):
+            h, w = (int(h * max_size / w), max_size)
+        size = (h, w)
 
-	transform = transforms.Compose([
-		transforms.Resize(size),
-		transforms.ToTensor(),
-		transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-		])
+    transform = transforms.Compose([
+        transforms.Resize(size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
 
-	# remove the transparent (alpha) channel and add the batch (singleton) dimension
-	image = transform(image)[:3,:,:].unsqueeze(0)
-	
-	return image
+    # remove the transparent (alpha) channel and add the batch (singleton) dimension
+    image = transform(image)[:3,:,:].unsqueeze(0)
+    
+    return image
 
 
 def convert_image(tensor):
-	'''Converts image from a Tensor image to a NumPy image'''
+    '''Converts image from a Tensor image to a NumPy image'''
 
-	image = tensor.to('cpu').clone().detach()
-	image = image.numpy().squeeze() # remove single-dimensional entries from the shape of an array.
-	image = image.transpose(1, 2, 0)
-	image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
-	image = image.clip(0, 1) # just in case that some values lies ooutside the [0, 1] range
+    image = tensor.to('cpu').clone().detach()
+    image = image.numpy().squeeze() # remove single-dimensional entries from the shape of an array.
+    image = image.transpose(1, 2, 0)
+    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    image = image.clip(0, 1) # just in case that some values lies ooutside the [0, 1] range
 
-	return image
+    return image
 
 
 # Well, this function is a little overkill, but instructive :)
@@ -105,3 +105,13 @@ def get_features(image, model, layer_names):
             features[layer_names[i]] = x
             
     return features
+    
+
+def gram_matrix(tensor):
+    '''Calculate the Gram Matrix of a given tensor'''
+
+    _, depth, height, width = tensor.shape # discard the batch size
+    tensor = tensor.view(depth, height * width)
+    gram = torch.mm(tensor, tensor.t())
+   
+    return gram
